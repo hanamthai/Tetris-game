3a808eda04e161af0f3a287daf9a794f24fe706d
ThaiNamHa-windows <hanamthai02@gmail.com>
2022-08-09 15:36:39 +0700

    create tetrominos for letter and display it on the screen

--- tetris.py
+++ tetris.py
@@ -2,6 +2,7 @@ from turtle import Screen
 import pygame as pg
 import random as rd
 import time
+from dataclasses import dataclass
 
 pg.init()  # initialize all imported pygame modules
 width, columns, rows = 400, 15, 30
@@ -17,8 +18,34 @@ for i in range(8):
 
 screen = pg.display.set_mode([width, height])
 pg.display.set_caption('Tetris Game')
-grild[0] = 2
-grild[1] = 2
+
+# tetromino for letters O,I,J,L,S,Z,T (https://en.wikipedia.org/wiki/Tetromino)
+tetrominos = [[0, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0],  # O
+              [0, 0, 0, 0, 2, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0],  # I
+              [0, 0, 0, 0, 3, 3, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0],  # J
+              [0, 0, 4, 0, 4, 4, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0],  # L
+              [0, 5, 5, 0, 5, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],  # S
+              [6, 6, 0, 0, 0, 6, 6, 0, 0, 0, 0, 0, 0, 0, 0, 0],  # Z
+              [0, 0, 0, 0, 7, 7, 7, 0, 0, 7, 0, 0, 0, 0, 0, 0]]  # T
+
+# create class and define function
+
+
+@dataclass
+class tetromino():
+    tetro: list
+    row: int = 0    # create coordinates that start to fall
+    column: int = 5
+
+    def show(self):
+        for n, color in enumerate(self.tetro):
+            if color > 0:
+                x = (self.column + n % 4) * distance
+                y = (self.column + n//4) * distance
+                screen.blit(picture[color], (x, y))
+
+
+character = tetromino(tetrominos[6])
 
 status = True
 while status:
@@ -26,6 +53,7 @@ while status:
         if event.type == pg.QUIT:
             status = False
     screen.fill((128, 128, 128))  # background color
+    character.show()
     for i, color in enumerate(grild):
         if color > 0:
             x = i % columns * distance  # coordinates of the square
